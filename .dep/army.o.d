d76bdf3cbd9cdcc86bb5d38efd82a72cc085a4ce
Madhushalini Murali <muralimadhushalini@gmail.com>
2023-05-27 10:59:47 -0500

    Landmine detection

---
+++ .dep/army.o.d
@@ -0,0 +1,1 @@
+army.o: army.c
---
+++ army.c
@@ -0,0 +1,155 @@
+
+#include<avr/io.h>
+#include<util/delay.h>
+void main()
+{
+	DDRD=0b00000000;
+	DDRB=0b11111111;
+	PORTB=0b11111111;
+	int c;
+	while(1)
+	{
+		c=PIND;
+		if(c==0b11111111)
+				PORTB=0b00001010;
+		if(c==0b11111110)
+				PORTB=0b00000110;
+		if(c==0b11111101)
+		{		
+				PORTB=0b00000101;
+				_delay_ms(3000);
+				PORTB=0b00001001;
+				_delay_ms(3000);
+		}
+		if(c==0b11111100)
+		{
+				PORTB=0b00000101;
+				_delay_ms(3000);
+				PORTB=0b00000110;
+				_delay_ms(3000);
+		}
+		if(c==0b11111011)
+				PORTB=0b00001001;
+		if(c==0b11111010)
+		{
+				PORTB=0b00000101;
+				_delay_ms(4000);
+				PORTB=0b00001001;
+				_delay_ms(3000);
+		}
+		if(c==0b11111001)
+		{
+				PORTB=0b00000101;
+				_delay_ms(4000);
+				PORTB=0b00001001;
+				_delay_ms(3000);
+		}
+		if(c==0b11111000)
+		{
+				PORTB=0b00000101;
+				_delay_ms(3000);
+				PORTB=0b00001001;
+				_delay_ms(3000);
+		}
+	
+		// fourth sensor is detecting and 0-black light
+		
+		if(c==0b11110111)
+		{
+				PORTB=0b00010000;
+				_delay_ms(30000);
+				PORTB=0b00011010;
+		}
+		if(c==0b11110110)
+		{
+				PORTB=0b00010000;
+				_delay_ms(30000);
+				PORTB=0b00000110;
+				_delay_ms(3000);
+		}
+		if(c==0b11110101)
+		{		
+				PORTB=0b00010000;
+				_delay_ms(30000);
+				PORTB=0b00000101;
+				_delay_ms(3000);
+				PORTB=0b00000110;
+				_delay_ms(3000);
+		}
+		if(c==0b11110100)
+		{
+				PORTB=0b00010000;
+				_delay_ms(30000);
+				PORTB=0b00000101;
+				_delay_ms(3000);
+				PORTB=0b00000110;
+				_delay_ms(3000);
+		}
+		if(c==0b11110011)
+		{
+				PORTB=0b00010000;
+				_delay_ms(30000);
+				PORTB=0b00001001;
+				_delay_ms(3000);
+		}
+		if(c==0b11110010)
+		{
+		
+				PORTB=0b00010000;
+				_delay_ms(30000);
+				PORTB=0b00000101;
+				_delay_ms(3000);
+				PORTB=0b00000110;
+				_delay_ms(3000);
+		}
+		if(c==0b11110001)
+		{
+				PORTB=0b00010000;
+				_delay_ms(30000);
+				PORTB=0b00000101;
+				_delay_ms(3000);
+				PORTB=0b00001001;
+				_delay_ms(3000);
+		}
+		if(c==0b11110000)
+		{
+				PORTB=0b00010000;
+				_delay_ms(30000);
+				PORTB=0b00000101;
+				_delay_ms(3000);
+				PORTB=0b00001001;
+				_delay_ms(3000);
+		}
+	}
+}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+				
+				
